c491cf6415eb2fe6c26ca901c92e7241bed9fdbe
santiagojalzatem-sudo <santiagoj.alzatem@uqvirtual.edu.co>
2025-10-29 15:50:54 -0500

    update clase usuario

--- Gimnasio/src/main/java/uniquindio/edu/co/model/Usuario.java
+++ Gimnasio/src/main/java/uniquindio/edu/co/model/Usuario.java
@@ -1,17 +1,12 @@
 package uniquindio.edu.co.model;
 
 public class Usuario {
-private String nombre;
-private String identificacion;
-private int edad;
-private String telefono;
-<<<<<<< HEAD
-private String tipo;
-=======
 
-
->>>>>>> 02de2094fdd86b35e704a4989da7ec88b6457297
-
+    private String nombre;
+    private String identificacion;
+    private int edad;
+    private String telefono;
+    private String tipo;
 
 
 
